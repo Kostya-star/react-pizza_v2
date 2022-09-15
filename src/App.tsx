4be138c30592fe7efe5e736cab15a9f3b82f705c
Constantin <konstantindanilov19@hotmail.com>
2022-09-15 22:25:02 +0300

    react-pizza_v2 #21 adjusting the whole app to typescript, added tsconfig.json

--- src/App.tsx
+++ src/App.tsx
@@ -11,18 +11,19 @@ import MainLayout from './components/layouts/MainLayout.jsx';
 
 
 const App = () => {
-  return ( 
-      
-          <Routes>
-            <Route path='/' element={ <MainLayout/> }>
-              <Route path='' element={ <Home/> } />
-              <Route path='cart' element={ <Cart/> } />
-              <Route path='pizzas/:id' element={ <FullPizza/> } />
-              <Route path='*' element={ <NotFound/> } />
-            </Route>
-          </Routes>
+  return (
+    <Routes>
+      <Route path='/' element={ <MainLayout/> }>
+        <Route path='' element={ <Home/> } />
+        <Route path='cart' element={ <Cart/> } />
+        <Route path='pizzas/:id' element={ <FullPizza/> } />
+        <Route path='*' element={ <NotFound/> } />
+      </Route>
+    </Routes>
       
   );
 }
 
+
 export default App;
+
--- src/index.tsx
+++ src/index.tsx
@@ -20,3 +20,5 @@ if(rootElem) {
     </BrowserRouter>
   );
 }
+
+
